f50539ae5f7ec8da1fb953a1fc4e7f41e7cbed3e
Artboomy <dg.danis.gumerov@gmail.com>
2021-11-05 13:50:40 +0500

    upgrade ts

--- src/utils.ts
+++ src/utils.ts
@@ -10,22 +10,17 @@ export default function downloadAsFile(
     type = 'text/plain'
 ): void {
     const file = new Blob([data], { type });
-    if (window.navigator.msSaveOrOpenBlob)
-        // IE10+
-        window.navigator.msSaveOrOpenBlob(file, filename);
-    else {
-        // Others
-        const a = document.createElement('a'),
-            url = URL.createObjectURL(file);
-        a.href = url;
-        a.download = filename;
-        document.body.appendChild(a);
-        a.click();
-        setTimeout(function () {
-            document.body.removeChild(a);
-            window.URL.revokeObjectURL(url);
-        }, 0);
-    }
+    // Others
+    const a = document.createElement('a'),
+        url = URL.createObjectURL(file);
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    setTimeout(function () {
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+    }, 0);
 }
 
 export function isIframeEvent(
